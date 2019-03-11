d30c250185545d03d01c73b2075047a8ec912981
Malcolm Fraser <MalcolmF@imqs.local>
2019-03-11 15:38:08 +0200

    Added new tags and changes

--- docker_tag_repo.rb
+++ docker_tag_repo.rb
@@ -14,18 +14,31 @@ class PromoteReleaseCandidate
     release_candidates = []
     release_candidates[0] = json
 
-    release_candidates.each{|repo|
+    release_candidates.each { |repo|
       # Store the json oblects in varaibles
       repository_name = repo["GitRepo"]
       docker_service_name = repo["DockerServiceName"]
       docker_buils_command = repo["DockerBuild"]
 
       #Clone repo locally
+      repo_dir = 'mm'
       # Todo : Get from json
-      Git.clone(repository_name, 'mm')
+      git_repo = Git.clone(repository_name, repo_dir) unless File.directory?(repo_dir)
+      git_repo = Git.open (repo_dir)
 
-      # Get the build number
 
+      `cd #{repo_dir}`
+      begin
+        # Get the build number
+        version_number = `git describe --tags`.to_f
+        version_number = version_number + 1
+        puts(`git tag #{version_number}`)
+      rescue
+        version_number = `git tag 1.0`.to_f
+      end
+
+      output = `git push --tags`
+      puts (output);
 
       # If there is no build number <tag> : Create one.
 
