6991db028cf285b9dbfd15092151c79307e7c7b6
Malcolm Fraser <MalcolmF@imqs.local>
2019-03-11 13:33:43 +0200

    testing commits

--- docker_tag_repo.rb
+++ docker_tag_repo.rb
@@ -1,20 +1,38 @@
-require ('json')
+require 'json'
+require 'git'
 
 class PromoteReleaseCandidate
 
 
   def self.make_permalink()
-          json = JSON.parse('{
+    json = JSON.parse('{
           "GitRepo" : "git@github.com:IMQS/mm.git",
           "DockerServiceName" : "imqs/mm:ITOOLS603-MF-CreateReleaseCandidate",
           "DockerBuild" : "sudo docker build -t imqs/mm --build-arg netrc=\"$(cat ~/.netrc)\" --build-arg ssh_prv_key=\"$(cat ~/.ssh/id_rsa)\" --build-arg ssh_pub_key=\"$(cat ~/.ssh/id_rsa.pub)\" ."
 }')
 
+    release_candidates = []
+    release_candidates[0] = json
 
-    # Store the json oblects in varaibles
-    repo = json["GitRepo"]
-    docker_service_name = json["DockerServiceName"]
-    docker_buils_command = json["DockerBuild"]
+    release_candidates.each{|repo|
+      # Store the json oblects in varaibles
+      repository_name = repo["GitRepo"]
+      docker_service_name = repo["DockerServiceName"]
+      docker_buils_command = repo["DockerBuild"]
+
+      #Clone repo locally
+      # Todo : Get from json
+      Git.clone(repository_name, 'mm')
+
+      # Get the build number
+
+
+      # If there is no build number <tag> : Create one.
+
+      # Else increment existing
+
+      # Then build
+    }
   end
 end
 
