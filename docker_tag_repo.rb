ad936ba2ac65928e2eb9db897e63a9472c44879b
Malcolm Fraser <MalcolmF@imqs.local>
2019-03-11 10:56:16 +0200

    testing token

--- docker_tag_repo.rb
+++ docker_tag_repo.rb
@@ -8,7 +8,6 @@ class PromoteReleaseCandidate
           "GitRepo" : "git@github.com:IMQS/mm.git",
           "DockerServiceName" : "imqs/mm:ITOOLS603-MF-CreateReleaseCandidate",
           "DockerBuild" : "sudo docker build -t imqs/mm --build-arg netrc=\"$(cat ~/.netrc)\" --build-arg ssh_prv_key=\"$(cat ~/.ssh/id_rsa)\" --build-arg ssh_pub_key=\"$(cat ~/.ssh/id_rsa.pub)\" ."
-          "LatestVersionMarkedForRelease"
 }')
 
     puts (json)
