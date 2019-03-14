41a8ccd6f1623441e7378c7799d558cc28cb8dbe
Malcolm Fraser <MalcolmF@imqs.local>
2019-03-14 13:01:14 +0200

    Added some changes.

--- docker_tag_repo.rb
+++ docker_tag_repo.rb
@@ -1,57 +1,88 @@
 require 'json'
+require 'Open3'
 
 class PromoteReleaseCandidate
-  def self.make_permalink()
-    json = JSON.parse('{
-          "GitRepo" : "git@github.com:IMQS/mm.git",
-          "DockerServiceName" : "imqs/mm:ITOOLS603-MF-CreateReleaseCandidate",
-          "DockerBuild" : "sudo docker build -t imqs/mm --build-arg netrc=\"$(cat ~/.netrc)\" --build-arg ssh_prv_key=\"$(cat ~/.ssh/id_rsa)\" --build-arg ssh_pub_key=\"$(cat ~/.ssh/id_rsa.pub)\" ."
-}')
-
-    puts ("start the scriopt.")
-    release_candidates = []
-    release_candidates[0] = json
+  def self.prepareReleaseCandidate()
+    release_candidates = fetch_release_candidates ("releaseCandidates.json")
 
     puts ("We are about to itterate throu the json objects.")
     release_candidates.each { |repo|
       # Store the json oblects in varaibles
-      repository_name = repo["GitRepo"]
       docker_service_name = repo["DockerServiceName"]
-      docker_buils_command = repo["DockerBuild"]
+      docker_build_command = repo["DockerBuild"]
+      repository_name = repo["GitRepo"]
 
-      #Clone repo locally
-      puts ("This is the where we specify the directory.")
-      repo_dir = 'mm'
-
-      if !File.directory?(repo_dir)
-        puts `mkdir #{repo_dir}`
-        puts `cd #{repo_dir}`
-      end
-       puts `git clone #{repository_name}`
-
-      # Get the build number
-      tag = `git describe --tags 2>&1`
-      puts tag
-      if (tag.to_s.include? "fatal: No names found, cannot describe anything")
-        puts(`git tag 1.0`)
-      else
-        if tag.tr("\n", "").is_float?
-          version_number = tag.tr("\n", "").to_f
-          version_number = version_number + 1
-          puts(`git tag #{version_number}`)
-        end
-      end
+      branch = docker_service_name[docker_service_name.index(":")+ 1..docker_service_name.length]
+      prepare_version(repository_name, branch)
 
       # Then build
-      puts (`#{docker_buils_command}`)
+      puts (`#{docker_build_command}`)
     }
+  end
+
+  def self.prepare_version(repository_name, branch)
+    repository = GitRepository.new(repository_name, branch)
+    tag = repository.fetch_latest_tag
+    puts tag
+    if (tag.to_s.include? "fatal: No names found, cannot describe anything")
+      puts(`git tag v1.0`)
+    else
+      if tag.tr("\n", "").tr("v", "").is_float?
+        version_number = tag.tr("\n", "").tr("v", "").to_f
+        version_number = version_number + 1
+        puts(`git tag v#{version_number}`)
+      end
+    end
+  end
+
+  def self.fetch_release_candidates (file)
+    json_from_file = File.read(file)
+    return JSON.parse(json_from_file)
+  end
+end
+
+class GitRepository
+  VERSION_PREFIX = 'v'.freeze
+
+  def initialize(repository_name, branch)
+    #ToDo : fetch repo folder from the
+    repository_folder = repository_name[repository_name.index("/")+1..repository_name.index(".git") - 1]
+
+    if !File.directory?(repository_folder)
+      cmd = "git clone -b #{branch} #{repository_name}"
+      system(cmd, out: $stdout, err: :out)
+      gitrepo = Dir.new(repository_folder)
+    end
+    gitrepo = Dir.chdir(repository_folder)
+
+    return gitrepo
+  end
+
+  # Fetches git tags from the command line and outputs as stdout
+  def fetch_tags
+    tag = tag("-l \"#{VERSION_PREFIX}*\"  --sort=-#{VERSION_PREFIX}:refname").split("\n")
+    return tag
+  end
+
+  def fetch_latest_tag
+    version_array = fetch_tags
+    return version_array[0]
+  end
+
+  def tag(arg="")
+    return `git tag #{arg} 2>&1`
+  end
+
+  def push(arg="")
+    return `git push #{arg} 2>&1`
   end
 end
 
 class String
+  # ToDO : modify to include "v"
   def is_float?
     /\A[+-]?\d+[.]\d+\z/ === self
   end
 end
 
-PromoteReleaseCandidate.make_permalink()
+PromoteReleaseCandidate.prepareReleaseCandidate()
