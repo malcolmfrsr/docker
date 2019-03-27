77bda02869f1376c36e5441127db3628a90fcc9b
Malcolm Fraser <MalcolmF@imqs.local>
2019-03-27 11:18:18 +0200

    First commit

--- ImqsGit.rb
+++ ImqsGit.rb
@@ -1,75 +1,75 @@
 ##
-#Creats a git repositoy object, which has various git commands
-# @param : Valid git repository ie git@github.com:<orginization>/<repository>.git.
+#Creats a git repository object, which has various git commands
+# @param : Valid git repository ie git@github.com:<organization>/<repository>.git.
 # @param : Name of the branch you would like to checkout on clone
 # @param : Optional argument that the local path of the repository.
 class GitRepository
   attr_reader :branch, :head, :name, :version
-  VERSION_PREFIX = 'v'.freeze
 
-  def initialize(repository_name, branch, folder="")
+  def initialize(repository_name, branch, folder='')
     # Fetch the folder from the name, if it is not specified.
     if folder.empty?
-      repository_folder = repository_name[repository_name.index("/")+1..repository_name.index(".git") - 1]
+      repository_folder = repository_name[repository_name.index('/')+1..repository_name.index('.git') - 1]
     else
       repository_folder = folder
     end
 
     # If the directory does not exist, assume that it has not been cloned.
-    if !File.directory?(repository_folder)
+    unless File.directory?(repository_folder)
       cmd = "git clone -b #{branch} #{repository_name}"
       system(cmd, out: $stdout, err: :out)
-      gitrepo = Dir.new(repository_folder)
+      Dir.new(repository_folder)
     end
-    gitrepo = Dir.chdir(repository_folder)
+    Dir.chdir(repository_folder)
 
     @branch = branch # The branch that git repo will be on.
-    @head = fetch_revision("HEAD") # The current head.
-    @name = repository_name # The name of the repossitory.
+    @head = fetch_revision('HEAD') # The current head.
+    @name = repository_name # The name of the repository.
     @version = fetch_latest_tag # The last tagged version.
   end
 
   # Fetch the revision specified.
   # @param : Takes either HEAD or <tags> as string.
-  # @returns : The revsion number as a string.
+  # @returns : The revision number as a string.
   def fetch_revision(commit)
-    return `git rev-parse #{commit}`.tr("\n", "")
+    `git rev-parse #{commit}`.tr("\n", '')
   end
 
-  # Tags a git reopsitory and and outputs to stdout.
+  # Tags a git repository and and outputs to stdout.
   # @param : accepts valid tag arguments.
   # @returns standard out.
-  def tag(arg="")
-    return `git tag #{arg} 2>&1`
+  def tag(arg='')
+    `git tag #{arg} 2>&1`
   end
 
   # Does a 'git push'.
   # @param : accepts valid tag arguments.
   # @returns : standard out.
-  def push(arg="")
-    return `git push #{arg} 2>&1`
+  def push(arg='')
+    `git push #{arg} 2>&1`
   end
 
   # Fetches all the valid tags for repository branch.
   # Validates if the version name conforms to v*.*
-  # ToDo : discuss version tag formatting.
   # @param : accepts valid tag arguments.
-  def fetch_tags (arg)
+  def fetch_tags (arg, regex=/\A[v]\d+[.]\d+\z/)
     clean_tags = []
     tag(arg).split("\n").each { |vtag|
-      clean_tags.push(vtag) if vtag.match(/\A[v]\d+[.]\d+\z/)
-    }
+      clean_tags.push(vtag) if vtag.match(regex) }
     # contains tags that conform to format : discuss
-    return clean_tags
+    return clean_tags unless clean_tags.length < 1
+    raise ImqsGitError, "no tags were found that matches the regex : #{regex.to_s}"
   end
 
   # Fetches the last version.
-  def fetch_latest_tag
-    version_array = fetch_tags("-l \"#{VERSION_PREFIX}*\"  --sort=-#{VERSION_PREFIX}:refname")
-    if version_array.length > 0
-    return version_array[0]
-    else
-      raise StandardError
-    end
+  def fetch_latest_tag(version_prefix = 'v')
+    version_array = fetch_tags("-l \"#{version_prefix}*\"  --sort=-#{version_prefix}:refname")
+    return version_array[0] unless version_array.length < 1
+    # raise error if empty
+    raise ImqsGitError, "No tags were found for prefix#{version_prefix}"
   end
+end
+
+# Custom error.
+class ImqsGitError < StandardError
 end
--- docker_tag_repo.rb
+++ docker_tag_repo.rb
@@ -2,53 +2,95 @@ require 'json'
 require_relative 'ImqsGit'
 
 class PromoteReleaseCandidate
-  def self.prepareReleaseCandidate()
-    release_candidates = fetch_release_candidates ("releaseCandidates.json")
-
-    puts ("We are about to itterate throu the json objects.")
+  # The main runner method to be called
+  def self.prepare_release_candidate
+    release_candidates = fetch_release_candidates ('releaseCandidates.json')
+    manifest_json = []
+    #Starting release candidate process.
     release_candidates.each { |repo|
-      # Store the json objects in varaibles
-      docker_service_name = repo["DockerServiceName"]
-      docker_build_command = repo["DockerBuild"]
-      repository_name = repo["GitRepo"]
+      # Get properties from json file.
+      docker_service_name = repo['DockerServiceName']
+      docker_build_command = repo['DockerBuild']
+      repository_name = repo['GitRepo']
+      # Get the name of the branch. Should always be master, but adding this option for development purposes.
+      branch = docker_service_name[docker_service_name.index(':')+ 1..docker_service_name.length]
+      # Get the version. Versioning calculations happen in here. If the version stays the same the code will successfully exit(0)
+      version = prepare_version(repository_name, branch)
+      # ToDo : Set condition not to build if nothing has changed.
+      prepare_docker_image(docker_build_command)
+      # Create a service hash to be used in json object, for the manifest file.
+      temp_hash = create_service_hash(docker_service_name, version)
+      manifest_json.push(temp_hash) }
 
-      branch = docker_service_name[docker_service_name.index(":")+ 1..docker_service_name.length]
-      puts(prepare_version(repository_name, branch))
+    # Manifest file created here.
+    create_manifest_file(manifest_json)
+  end
 
-      # Then build
-      puts ("ToDo : do a docker build here")
+  private
+  # Creates a physical file called manifest.json
+  # Writes to the the above mentioned file, the content of the hash as json.
+  # @param : Ruby hash.
+  def self.create_manifest_file(manifest_json)
+    file_name = '../manifest.json'
+    File.open(file_name, 'w') do |f|
+      f.write(manifest_json.to_json)
+    end
+  end
 
-      # Then build
-      puts ("ToDo : create manifest")
+  # Build the docker image and pushes if successful.
+  # @param : A docker build command.
+  def self.prepare_docker_image(docker_build_command)
+    std_out = `#{docker_build_command} 2>&1`
+    puts (std_out)
+    # ToDo : Docker Push
+  end
+
+  # Creates a ruby hash containing the following
+  # @param1 : The docker image service name.
+  # @param2 : The version number as an integer, of the image.
+  def self.create_service_hash(docker_service_name, version)
+    {
+        :ServiceName => docker_service_name,
+        :Version => version
     }
   end
 
-  def self.prepare_version(repository_name, branch)
-    repository = GitRepository.new(repository_name, branch)
-    puts (repository.version)
-    # TODO : I think I need to move this internally and surface as an attribute. (Thinking of the negative implication.)
-    if ((repository.head) == (repository.fetch_revision(repository.version)))
-      puts "Nothing has changed. Keeping #{repository.version}"
-      return repository.version
-    end
-
-    # TODO : This could also be a possible attr on the repository class.
-    if (repository.version.to_s.include? "fatal: No names found, cannot describe anything")
-      repository.tag("v1.0")
-    else
-      # TODO : This as well ?
-      if repository.version.tr("\n", "").tr("v", "").is_float?
-        version_number = repository.version.tr("\n", "").tr("v", "").to_i
-        version_number = (version_number.to_i + 1).to_f
-        repository.tag("v#{version_number}")
-        return version_number
-      end
-    end
-  end
-
+  # Fetch the release candidated from a specified json file.
+  # @param :
   def self.fetch_release_candidates (file)
     json_from_file = File.read(file)
     return JSON.parse(json_from_file)
+  end
+
+  # Does the major version calculation, if anything has changed.
+  #
+  def self.prepare_version(repository_name, branch)
+    repository = GitRepository.new(repository_name, branch)
+    puts (repository.version)
+    if (repository.head) == (repository.fetch_revision(repository.version))
+      puts "Nothing has changed. Keeping #{repository.version}"
+      exit(0)
+    end
+
+    return provide_version(repository)
+  rescue ImqsGitError => error
+    puts("#{error.message} : #{error.backtrace}")
+    exit (1)
+  end
+
+  def self.provide_version(repository)
+    if repository.version.to_s.include? 'fatal: No names found, cannot describe anything'
+      repository.tag('v1.0')
+    else
+      increment_version(repository) if repository.version.tr("\n", '').tr('v', '').is_float?
+    end
+  end
+
+  def self.increment_version(repository)
+    version_number = repository.version.tr("\n", '').tr('v', '').to_i
+    version_number = (version_number.to_i + 1).to_f
+    repository.tag("v#{version_number}")
+    return version_number
   end
 end
 
@@ -58,4 +100,4 @@ class String
   end
 end
 
-PromoteReleaseCandidate.prepareReleaseCandidate()
+PromoteReleaseCandidate.prepare_release_candidate
